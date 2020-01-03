9bb403cec0b5ddbf87503ee518ad6b57cae929d2
QYQ323 <1725548486@qq.com>
2020-01-03 16:18:16 +0800

    AES

--- tkinter_AES.py
+++ tkinter_AES.py
@@ -61,12 +61,12 @@ btn_jiemi = tk.Button(window, text='解 密',font=('Arial', 10),width=5,command=
 btn_jiemi.place(x=625, y=65)
 
 # 第4步，在图形界面上创建 500 * 200 大小的画布并放置各种元素
-#canvas = tk.Canvas(window, height=200, width=700)
+canvas = tk.Canvas(window, height=200, width=700)
 # 说明图片位置，并导入图片到画布上
-#image_file = tk.PhotoImage(file='111.png')  # 图片位置（相对路径，与.py文件同一文件夹下，也可以用绝对路径，需要给定图片具体绝对路径）
-#image = canvas.create_image(625, 0, anchor='n', image=image_file)  # 图片锚定点（n图片顶端的中间点位置）放在画布（250,0）坐标处
+image_file = tk.PhotoImage(file='111.png')  # 图片位置（相对路径，与.py文件同一文件夹下，也可以用绝对路径，需要给定图片具体绝对路径）
+image = canvas.create_image(625, 0, anchor='n', image=image_file)  # 图片锚定点（n图片顶端的中间点位置）放在画布（250,0）坐标处
 
-#canvas.pack()
+canvas.pack()
 
 # 第6步，触发函数，用来一定指定图形
 '''
